5ebcb2f50961dbdd3bffe00ddb8c696d19f6ac15
Roman <rdymshakov@inbox.ru>
2023-04-19 08:03:38 +0300

    delete webhook, do simple echo bot

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/mymmrac/telego"
+	tu "github.com/mymmrac/telego/telegoutil"
 	"os"
 )
 
@@ -16,32 +17,25 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	updates, _ := bot.UpdatesViaLongPolling(nil)
+	defer bot.StopLongPolling()
 
-	// Set up a webhook on Telegram side
-	_ = bot.SetWebhook(&telego.SetWebhookParams{
-		URL: "https://example.com/bot" + bot.Token(),
-	})
-
-	// Receive information about webhook
-	info, _ := bot.GetWebhookInfo()
-	fmt.Printf("Weebhook Info %+v\n", info)
-
-	// Get updates channel from webhook
-	updates, _ := bot.UpdatesViaWebhook("/bot" + bot.Token())
-
-	//Start server for receiving request from the Telegram
-	go func() {
-		_ = bot.StartWebhook("localhost:443")
-	}()
-
-	// stop reviving updates from channel and shutdown webhook server
-	defer func() {
-		_ = bot.StopWebhook()
-	}()
-
-	// Loop through all updates when they came
 	for update := range updates {
-		fmt.Printf("Update %+v\n", update)
-	}
 
+		if update.Message != nil {
+			//retrive chat ID
+			chatID := update.Message.Chat.ID
+
+			// call method sendmessage
+			// Send a message to sender with the same text(echo bot)
+			sentMessage, _ := bot.SendMessage(
+				tu.Message(
+					tu.ID(chatID),
+					update.Message.Text,
+				),
+			)
+			fmt.Printf("Setn Message: %v\n", sentMessage)
+
+		}
+	}
 }
